6ad152289f8ee61a19763120bb1fbb34854a516a
Spacefox95 <nathan.raynal@gmail.com>
2025-06-13 09:50:07 +0200

    ok

---
+++ utils/redis.mjs
@@ -0,0 +1,40 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this._client = createClient();
+    this._client.on('error', (err) => console.error(err));
+  }
+
+  isAlive() {
+    if (this._client) return true;
+    return false;
+  }
+
+  async get(key) {
+    try {
+      const value = await this._client.get(key);
+      if (value) return value;
+      return null;
+    } catch (err) {
+      console.error(`Error getting key "${key}":`, err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      this._client.set(key, String(value), 'EX', duration);
+      console.log(value);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async del(key) {
+    this._client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
