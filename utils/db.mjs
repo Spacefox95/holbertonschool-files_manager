b820d7149f275711d040ecb07f07293f14b047ab
Spacefox95 <nathan.raynal@gmail.com>
2025-06-13 11:11:15 +0200

    ok

---
+++ utils/db.mjs
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}`;
+    this._client = new MongoClient(uri);
+
+    this._client
+      .connect()
+      .then(() => {
+        this.db = this._client.db(database);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  isAlive() {
+    return this._client && this._client.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.db) return 0;
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) return 0;
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
